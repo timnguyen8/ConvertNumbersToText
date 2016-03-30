aab01e19226640db60f0ec6ce5616be1ed0f73d6
timnguyen8 <tim_nguyen@outlook.com>
2016-03-30 19:22:31 +1000

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Number_To_Text_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputString = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtOutputString = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input The Number: ";
+            // 
+            // txtInputString
+            // 
+            this.txtInputString.Location = new System.Drawing.Point(44, 69);
+            this.txtInputString.Name = "txtInputString";
+            this.txtInputString.Size = new System.Drawing.Size(336, 29);
+            this.txtInputString.TabIndex = 1;
+            this.txtInputString.Text = "$ 00.00";
+            this.txtInputString.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtInputString.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtInputString_MouseClick);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(44, 129);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(197, 37);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert To Text";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtOutputString
+            // 
+            this.txtOutputString.HideSelection = false;
+            this.txtOutputString.Location = new System.Drawing.Point(44, 196);
+            this.txtOutputString.Multiline = true;
+            this.txtOutputString.Name = "txtOutputString";
+            this.txtOutputString.Size = new System.Drawing.Size(618, 104);
+            this.txtOutputString.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 322);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtOutputString);
+            this.Controls.Add(this.txtInputString);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(720, 360);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(720, 360);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NumbersToText Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputString;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtOutputString;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_To_Text_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInputString_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtInputString.Clear();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            char ch = '.';
+            string integerString = "", fractionalString = "", result = "";
+            string cent = "Cent", dollar = "Dollar";
+            double inputDouble = 0.0;
+
+            if ((txtInputString.Text == "")|| txtInputString.Text =="$00.00") { txtInputString.Text = "0.00"; };
+            string inputValue = txtInputString.Text;           
+
+            inputDouble = double.Parse(inputValue);
+            if (inputDouble < 0)
+            {
+                txtOutputString.Text = "Input Number should not be negative, Please try again!!";
+            }
+            else if (inputDouble > 1000000000)
+            {
+                txtOutputString.Text = "Input Number is too big, Please try again!!";
+            }
+            else
+            {
+
+
+                int inputLength = inputValue.Length;
+                //find the index of "." in input value 
+                int indexOfDot = inputValue.IndexOf(ch);
+
+                //if input without fraction
+                if (!inputValue.Contains(ch))
+                {
+                    fractionalString = "00";
+                    integerString = inputValue;
+                }
+                else
+                {
+                    fractionalString = inputValue.Substring(indexOfDot + 1);
+
+                    if (fractionalString.Length == 0)
+                    {
+                        inputValue += "00";
+                        fractionalString = "00";
+                        integerString = inputValue.Substring(0, inputLength - 1);
+                    }
+                    else if (fractionalString.Length == 1)
+                    {
+
+                        fractionalString = inputValue.Substring(indexOfDot + 1) + "0";
+                        integerString = inputValue.Substring(0, inputLength - 2);
+                    }
+                    else if (fractionalString.Length == 2)
+                    {
+                        integerString = inputValue.Substring(0, inputLength - 3);
+                        fractionalString = inputValue.Substring(indexOfDot + 1, 2);
+
+                    }
+                    else if (fractionalString.Length > 2)
+                    {
+                        integerString = inputValue.Substring(0, inputLength - fractionalString.Length - 1);
+                        fractionalString = inputValue.Substring(indexOfDot + 1, 2);
+
+                    }
+
+
+                }
+
+
+                //Convert two strings to interger value
+                int integerPart = int.Parse(integerString);
+                int fractionalPart = int.Parse(fractionalString);
+
+
+                //Cents or Cent and Nothing 
+                if (fractionalPart == 0) { cent = ""; }
+                else if (fractionalPart > 1) { cent += "s"; };
+                //Dollars or Dollar And Nothing
+                if (integerPart == 0) { dollar = ""; }
+                else if (integerPart > 1) { dollar += "s"; };
+
+                //Format output 
+
+                if ((fractionalPart == 0) && (integerPart != 0))
+                {
+                    result = string.Format("{0:C2}: \t{1} {2}",Double.Parse(integerString + "." + fractionalString), convertNumberToText(integerPart), dollar);
+                }
+                else if ((fractionalPart != 0) && (integerPart == 0))
+                {
+                    result = string.Format("{0:C2}: \t{1} {2}", Double.Parse(integerString + "." + fractionalString), convertNumberToText(fractionalPart), cent);
+                }
+                else if ((fractionalPart == 0) && (integerPart == 0))
+                {
+                    result = string.Format("{0:C2}: \t{1} {2}",Double.Parse(integerString + "." + fractionalString), convertNumberToText(integerPart), dollar);
+                }
+                else
+                {
+                    result = string.Format("{0:C2}: \t{1} {2} And {3} {4}",Double.Parse(integerString + "." + fractionalString), convertNumberToText(integerPart), dollar, convertNumberToText(fractionalPart), cent);
+                    ;
+
+                }
+
+                txtOutputString.Text = result.ToString();
+            }
+        }
+
+        /* 
+         *Using recursion to convert data in chunks of input
+         */
+
+        private string convertNumberToText(int value)
+        {
+
+            const int HUNDRED = 100, THOUSAND = 1000, MILLON = 1000000, BILLION = 1000000000;
+
+            if (value < HUNDRED)
+            {
+                switch (value)
+                {
+                    case 0: return "Zero";
+                    case 1: return "One";
+                    case 2: return "Two";
+                    case 3: return "Three";
+                    case 4: return "Four";
+                    case 5: return "Five";
+                    case 6: return "Six";
+                    case 7: return "Seven";
+                    case 8: return "Eight";
+                    case 9: return "Nine";
+                    case 10: return "Ten";
+                    case 11: return "Eleven";
+                    case 12: return "Twelve";
+                    case 13: return "Thirteen";
+                    case 14: return "Fourteen";
+                    case 15: return "Fifteen";
+                    case 16: return "Sixteen";
+                    case 17: return "Seventeen";
+                    case 18: return "Eighteen";
+                    case 19: return "Nineteen";
+                    case 20: return "Twenty";
+                    case 30: return "Thirty";
+                    case 40: return "Forty";
+                    case 50: return "Fifty";
+                    case 60: return "Sixty";
+                    case 70: return "Seventy";
+                    case 80: return "Eighty";
+                    case 90: return "Ninety";
+                    
+                    default:
+                        int numberPart = value % 10;
+                        int numberX10Part = value - numberPart;
+                        return convertNumberToText(numberX10Part) + "-" + convertNumberToText(numberPart);
+
+                }
+            }
+            else if (value < THOUSAND)
+            {
+                int hundredPart = value / HUNDRED;
+                int hundredRemain = value % HUNDRED;
+                if (hundredRemain == 0)
+                {
+                    return convertNumberToText(hundredPart) + " Hundred ";
+                }
+                else
+                {
+                    return convertNumberToText(hundredPart) + " Hundred and " + convertNumberToText(hundredRemain);
+                }
+            }
+            else if (value < MILLON)
+            {
+                if ((value % THOUSAND) == 0)
+                {
+                    return convertNumberToText(value / THOUSAND) + " Thousand ";
+                }
+                else
+                {
+                    return convertNumberToText(value / THOUSAND) + " Thousand, " + convertNumberToText(value % THOUSAND);
+                }
+
+                
+            }
+            else if (value < BILLION)
+            {
+                if ((value % THOUSAND) == 0)
+                {
+                    return convertNumberToText(value / MILLON) + " Million ";
+                }
+                else
+                {
+                    return convertNumberToText(value / MILLON) + " Million, " + convertNumberToText(value % MILLON);
+                }
+                
+            }
+            else
+            {
+                return "Out of Range";
+            }
+
+        }  
+    }
+}
